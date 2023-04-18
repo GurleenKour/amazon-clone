d22bf36cfcf873b41ee811a62bac2f479ffe9cf1
GurleenKour <gurleen.kour97@gmail.com>
2023-04-19 00:40:15 +0530

    initial project setup

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+// import { Inter } from 'next/font/google'
+
+// const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+    </div>
+  )
+}
