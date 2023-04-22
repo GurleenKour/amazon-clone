4f7f4e6ff094afa18e129d9e1143e2533d8b3e40
GurleenKour <gurleen.kour97@gmail.com>
2023-04-23 01:19:28 +0530

    learned to customize styling in tailwing & listed products from api.

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,71 @@
-import React from 'react'
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 // import {SearchIcon,MenuIcon,ShoppingCartIcon} from '@heroicons/react/24/outline';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import {
+  MagnifyingGlassIcon,
+  ShoppingCartIcon,
+  Bars3Icon
+} from "@heroicons/react/24/outline";
 function Header() {
   return (
     <header>
-        {/* top nav */}
-        {/*  */}
-      <div className='flex item-center bg-amazon_blue p-1 flex-grow py-2'>
-        <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
-            {/* use next ja image */}
-            <Image
-            src='https://links.papareact.com/f90'
+      {/* top nav */}
+      {/*  bg-amazon_blue- custom themem added in tailwind config */}
+      <div className="flex item-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          {/* use next ja image */}
+          <Image
+            src="https://links.papareact.com/f90"
             width={150}
             height={40}
-            objectFit='contain'
-            className='cursor-pointer'/>
-
+            objectFit="contain"
+            className="cursor-pointer"
+          />
         </div>
         {/* search */}
-        <div className='bg-yellow-400'>
-          <input type="text"/>
-          <MagnifyingGlassIcon className='h-12 p-4'/>
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 px-4 flex-grow flex-shrink rounded-l-md focus:outline-none"
+            type="text"
+          />
+          <MagnifyingGlassIcon className="h-12 p-4" />
+        </div>
+        {/* right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello gurleen!!</p>
+            <p className='font-extrabold md:text-sm'>nAccount nd lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className='font-extrabold md:text-sm'>and orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 rounded-full text-black text-center font-bold">{4}</span>
+            <ShoppingCartIcon className=' h-10' />
+            <p className='hidden md:inline font-extrabold md:text-sm mt-2 '>basket</p>
+          </div>
         </div>
       </div>
       {/* bottom nav */}
-      <div></div>
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white 
+      text-sm">
+        {/* <Menu */}
+        <p className="link flex items-center">
+          <Bars3Icon className="h-6 mr-1"/>
+          All</p>
+          <p className="link">Prome Video</p>
+          <p className="link">Amazon Business</p>
+          <p className="link">Today's deals</p>
+          <p className="hidden link lg:inline-flex">Electronics</p>
+          <p className="hidden link lg:inline-flex">Food & Delivery</p>
+          <p className="hidden link lg:inline-flex">Prime</p>
+          <p className="hidden link lg:inline-flex">Buy again</p>
+          <p className="hidden link lg:inline-flex">Shopper toolkit</p>
+          <p className="hidden link lg:inline-flex">Helath & personal care</p>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,33 @@
 // const inter = Inter({ subsets: ['latin'] })
 import Head from "next/head"
 import Header from "../components/header"
+import Banner from "@/components/Banner"
+import ProductsFeed from "@/components/ProductsFeed"
 
-export default function Home() {
+export default function Home({products}) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
       {/* <h1>hi everyone</h1> */}
       <Header/>
+      <main className="max-w-screen-lg mx-auto">
+        {/* Banner */}
+        <Banner/>
+
+      {/* Product feed */}
+      <ProductsFeed products={products}/>
+      </main>
     </div>
   )
+}
+// getServerSideProps- this tells next js that this page is no longer static, please render the page on server first then send it to the browser
+export async function getServerSideProps(context){
+ const products =await fetch('https://fakestoreapi.com/products').then(res=> res.json())
+ return {
+  props : {
+    products,
+  }
+ }
 }
